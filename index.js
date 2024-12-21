ec113907db769eace3cc5b52f2a6f97633318fac
Cestercian <yashafaid@gmail.com>
2024-12-21 15:50:29 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let deckId
+const cardsContainer = document.getElementById("cards")
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+            cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `
+        })
+})
