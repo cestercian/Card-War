306e8073fec8896c9ac8e27679a952ede75b7eb1
Cestercian <yashafaid@gmail.com>
2024-12-21 17:34:44 +0530

    UPDATE v.01

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 
 function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id
@@ -14,14 +14,31 @@ function handleClick() {
 newDeckBtn.addEventListener("click", handleClick)
 
 drawCardBtn.addEventListener("click", () => {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
             cardsContainer.children[0].innerHTML = `
-                <img src=${data.cards[0].image} class="card" />
+                <img src=${data.cards[0].image} class="card"  alt=""/>
             `
             cardsContainer.children[1].innerHTML = `
-                <img src=${data.cards[1].image} class="card" />
+                <img src=${data.cards[1].image} class="card"  alt=""/>
             `
+            winO(data.cards[0].value, data.cards[1].value)
         })
 })
+
+
+function winO (card1, card2){
+    const rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+
+    let c1 = rank.indexOf(card1);  // c1 is now a number (index of card1)
+    let c2 = rank.indexOf(card2);  // c2 is now a number (index of card2)
+
+    if(c1>c2){
+        console.log("Computer ki maa ka bhosda")
+    }else if (c2>c1){
+        console.log("You win")
+    }else{
+        console.log("draw")
+    }
+}
